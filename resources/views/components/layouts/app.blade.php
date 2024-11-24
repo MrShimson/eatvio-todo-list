ebfa820811f745ec897936dc203d9ec1405554a2
Semyon Nikiforov <semnikiforov2000@gmail.com>
2024-11-24 03:18:55 +0300

    Small fixes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -42,47 +42,6 @@
         </main>
 
         @livewireScripts
-        <script>
-            let root = document.querySelector('[drag-root]')
-            root.querySelectorAll('[drag-item]').forEach(el => {
-                el.addEventListener('dragstart', e => {
-                    e.target.setAttribute('dragging', true)
-                })
-
-                el.addEventListener('dragenter', e => {
-                    e.target.classList.add('bg-secondary')
-
-                    e.preventDefault()
-                })
-
-                el.addEventListener('dragover', e => {
-                    e.preventDefault()
-                })
-
-                el.addEventListener('dragleave', e => {
-                    e.target.classList.remove('bg-secondary')
-                })
-
-                el.addEventListener('dragend', e => {
-                    e.target.removeAttribute('dragging')
-                })
-
-                el.addEventListener('drop', e => {
-                    e.target.classList.remove('bg-secondary')
-
-                    let draggingEl = root.querySelector('[dragging]')
-                    e.target.before(draggingEl)
-
-                    let component = Livewire.find(
-                        e.target.closest('[wire\\:id]').getAttribute('wire:id')
-                    )
-
-                    let method = root.getAttribute('drag-root')
-                    let orderIds = Array.from(root.querySelectorAll('[drag-item]')).map(itemEl => itemEl.getAttribute('drag-item'))
-
-                    component.call(method, orderIds)
-                })
-            })
-        </script>
+        @stack('livewire-scripts')
     </body>
 </html>
