55301c1fa1d1f79607111ea3e91ab8d03b16edb3
Semyon Nikiforov <semnikiforov2000@gmail.com>
2024-11-27 12:39:17 +0300

    Small fixes

--- resources/views/livewire/list-users.blade.php
+++ resources/views/livewire/list-users.blade.php
@@ -7,15 +7,19 @@
                     <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                         <span class="visually-hidden">Toggle Dropdown</span>
                     </button>
-                    <div class="dropdown-menu p-0 mx-3">
-                        <div class="list-group">
-                            @foreach($usersTodoLists[$user->id] as $todoList)
-                                <a href="/todo-lists/{{ $todoList['id'] }}" class="list-group-item list-group-item-action text-decoration-none text-dark">
-                                    {{ $todoList['name'] }}
-                                </a>
-                            @endforeach
+                        <div class="dropdown-menu p-0 mx-3">
+                            <div class="list-group">
+                                @if (isset($usersTodoLists[$user->id]))
+                                    @foreach($usersTodoLists[$user->id] as $todoList)
+                                        <a href="/todo-lists/{{ $todoList['id'] }}" class="list-group-item list-group-item-action text-decoration-none text-dark">
+                                            {{ $todoList['name'] }}
+                                        </a>
+                                    @endforeach
+                                @else
+                                    <div class="list-group-item text-center">This User does not have any Todo Lists yet</div>
+                                @endif
+                            </div>
                         </div>
-                    </div>
                 </div>
             @endforeach
         </div>
